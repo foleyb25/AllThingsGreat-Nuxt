9804d786effc7061ee7d14a8f66cae7a7821d0a6
foleyb25 <foleyb25@gmail.com>
2023-08-20 17:16:22 -0600

    Removing content/smut headers on individual article

--- pages/articles/[slug].vue
+++ pages/articles/[slug].vue
@@ -34,46 +34,6 @@
         </div> -->
 
       </header>
-      <header class="grid grid-cols-3 gap-5 border-b border-black text-xs sm:text-xs md:text-sm lg:text-xl xl:text-xl">
-        <div class="flex items-center justify-center">
-          Content: {{ getArticle?.evaluation.content }}/5
-          <span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#ffff00" viewBox="0 0 24 24" stroke-width="1.5"
-              stroke="currentColor" class="w-4 h-4 lg:w-6 lg:h-6">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-            </svg>
-          </span>
-        </div>
-        <div class="flex items-center justify-center">
-          Structure: {{ getArticle?.evaluation.structure }}/5
-          <span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#ffff00" viewBox="0 0 24 24" stroke-width="1.5"
-              stroke="currentColor" class="w-4 h-4 lg:w-6 lg:h-6">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-            </svg>
-          </span>
-        </div>
-        <div class="flex items-center justify-center">
-          Organization: {{ getArticle?.evaluation.organization }}/5
-          <span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="#ffff00" viewBox="0 0 24 24" stroke-width="1.5"
-              stroke="currentColor" class="w-4 h-4 lg:w-6 lg:h-6">
-              <path stroke-linecap="round" stroke-linejoin="round"
-                d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
-            </svg>
-          </span>
-        </div>
-      </header>
-      <header class="flex flex-row justify-center mt-4">
-        <div class="w-full bg-gradient-to-r rounded from-[#007FFF] to-[#FF073A] relative text-center"> Smut
-          <div class="absolute top-0 mt-5 bg-black bg-opacity-75 h-full w-2 rounded"
-            :style="{ left: (getArticle?.evaluation.smut) * 10 + '%' }">
-            <div class="mt-5 flex justify-center">{{ getArticle.evaluation.smut }}/10</div>
-          </div>
-        </div>
-      </header>
       <div ref="target" class="relative mt-12">
         <img :src="getArticle?.imageUrl" alt="Blog image" class="rounded-xl w-full aspect-[3/2] object-cover" />
         <div class="rounded-xl absolute inset-0 bg-black bg-opacity-60"></div>
